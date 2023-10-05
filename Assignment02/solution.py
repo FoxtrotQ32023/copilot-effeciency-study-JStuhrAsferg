9a51297e95bd49b6c46ff824cfde45bf8f15925a
Jonathan Stuhr Asferg <58783059+JStuhrAsferg@users.noreply.github.com>
2023-10-05 08:43:34 +0000

    Pause

---
+++ Assignment02/solution.py
@@ -0,0 +1,28 @@
+import re
+file = open("/workspaces/copilot-effeciency-study-JStuhrAsferg/Assignment02/stringmultimatching.in")
+
+lines = file.readlines()
+print("lines: " + str(lines))
+strippedLines = []
+
+for i in lines:
+    #print (str(linenumber) + ":" + i)
+    strippedLines.append(i.strip('\n'))
+
+print("lines: " + str(strippedLines))
+
+
+currentLinenumber = 0
+keepRunning = True
+while keepRunning:
+    if currentLinenumber < len(strippedLines) and int(strippedLines[currentLinenumber]):
+
+        currentSearchStrings = strippedLines[currentLinenumber+1:currentLinenumber+int(strippedLines[currentLinenumber])+1]
+        currentSearch = strippedLines[currentLinenumber+int(strippedLines[currentLinenumber])+1]
+        currentLinenumber = currentLinenumber + int(strippedLines[currentLinenumber])+2
+        print("find: " + str(currentSearchStrings) + " In: " + currentSearch)
+        for searchText in currentSearchStrings:
+            indexesOfOccurences = [m.start() for m in re.finditer(searchText, currentSearch)]
+            print("Indexes of occurences: " + str(indexesOfOccurences))
+    else:
+        keepRunning = False
